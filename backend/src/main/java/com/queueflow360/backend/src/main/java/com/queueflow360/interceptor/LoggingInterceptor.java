224f49abc28beb00cdc90f603bb73bc0ddb666e6
Rachel R <rachelprincy55@gmail.com>
2026-07-19 22:15:24 +0530

    Create LoggingInterceptor.java

---
+++ backend/src/main/java/com/queueflow360/backend/src/main/java/com/queueflow360/interceptor/LoggingInterceptor.java
@@ -0,0 +1,9 @@
+package com.queueflow360.interceptor;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+}
