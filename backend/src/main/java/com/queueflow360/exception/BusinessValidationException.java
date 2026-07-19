33dad55e9ad320eedb4e499df79fb91845f2bfd9
Rachel R <rachelprincy55@gmail.com>
2026-07-19 22:10:50 +0530

    Add BusinessValidationException class

---
+++ backend/src/main/java/com/queueflow360/exception/BusinessValidationException.java
@@ -0,0 +1,8 @@
+package com.queueflow360.exception;
+
+public class BusinessValidationException extends RuntimeException {
+
+    public BusinessValidationException(String message) {
+        super(message);
+    }
+}
