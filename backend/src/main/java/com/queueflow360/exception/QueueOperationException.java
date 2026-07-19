ebaa8f1ed3721e29c9978f3a5901f9d958101d81
Rachel R <rachelprincy55@gmail.com>
2026-07-19 22:11:24 +0530

    Add QueueOperationException class for error handling

---
+++ backend/src/main/java/com/queueflow360/exception/QueueOperationException.java
@@ -0,0 +1,8 @@
+package com.queueflow360.exception;
+
+public class QueueOperationException extends RuntimeException {
+
+    public QueueOperationException(String message) {
+        super(message);
+    }
+}
